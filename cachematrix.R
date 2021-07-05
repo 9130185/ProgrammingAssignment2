29dabae25a304738d2a3e391e4e0bf81611603df
mah <khaleghi1360@yahoo.com>
2021-07-05 10:16:12 +0200

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,30 +5,41 @@
 ##set the value of the mean
 ##get the value of the mean
 ## this function can cache its inverse:
+library(MASS)
 
 makeCacheMatrix <- function(x = matrix()) {
-        inver <- NULL
+        inv <- NULL   #initializing inerse as NULL
         set <- function(y) {
                 x <<- y
-                inver <<- NULL
+                inv <<- NULL
         }
-        get <- function() x
-        setinverse <- function(inverse) inver <<- inverse
-        getinverse <- function() inver
-        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+        get <- function() x   #function to get matrix x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() {
+                inver <- ginv(x)
+                inver%*%x   #function to obtain inverse of the matrix
+        }
+        list(set = set,
+             get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 ##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 ##If the inverse has already been calculated (and the matrix has not changed), 
 ##then cacheSolve should retrieve the inverse from the cache.
-cacheSolve <- function(x, ...) {
-        inver <- x$getinverse()
-        if (!is.null(inver)) {
+cacheSolve <- function(x, ...) {  #gets cache data
+        inv <- x$getinv()
+        if (!is.null(inv)) {  #checking whether inverse in NULL
                 message("getting cached data")
-                return(inver)
+                return(inv)   #return inverse value
         }
         data <- x$get()
-        inver <- solve(data, ...)
-        x$setinverse(inver)
-        inver
-        
+        inv <- solve(data, ...)   #calculating inverse value
+        x$setinv(inv)
+        inv   #return a matrix that is the inverse of 'x'
 }
+
+f<- makeCacheMatrix(matrix(1:8, 2,4))
+f$get()
+f$getinv()
+cacheSolve(f)
